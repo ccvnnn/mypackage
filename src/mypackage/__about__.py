ff949b7b3f9d263f90fbf52b8d0bce0e08508ba3
can <125025906+ccvnnn@users.noreply.github.com>
2025-02-25 10:01:12 +0100

    Initial commit

---
+++ src/mypackage/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025-present can <125025906+ccvnnn@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2025-present can <125025906+ccvnnn@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
