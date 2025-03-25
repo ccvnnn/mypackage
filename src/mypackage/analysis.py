460882afaa86730db526c07459a14762fc8a8f5f
can <125025906+ccvnnn@users.noreply.github.com>
2025-03-25 10:55:00 +0100

    flake8 check

--- src/mypackage/analysis.py
+++ src/mypackage/analysis.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import pandas as pd
 import numpy as np
 import scipy
@@ -22,8 +22,8 @@ class Analyzer:
         Parameters
         ----------
         column: str
-            The name of the column to analyze. This should be a name of one 
-            of the columns in the Titanic dataset. 
+            The name of the column to analyze. This should be a name of one
+            of the columns in the Titanic dataset.
         
         
         Returns
@@ -31,7 +31,7 @@ class Analyzer:
         panda.Series
             A Series containing basic statistics about the specific column.
             
-            numerical variables use the following statistics:   
+            numerical variables use the following statistics:
             count: number of non-missing values
             mean: average of the values
             std: standard deviation
@@ -80,7 +80,7 @@ class Analyzer:
 
 
    
-    def cramers_v(self, chi2, contingency_table): 
+    def cramers_v(self, chi2, contingency_table):
         n = contingency_table.sum().sum()
         min_dim = min(contingency_table.shape) - 1
         return np.sqrt(chi2 / (n * min_dim))
@@ -88,57 +88,56 @@ class Analyzer:
     
     
     def chi_square_test(self, column1: str, column2: str):
-       """Does a Chi-Square test to analyze the relationship 
-       between two categorical variables.
+        """Does a Chi-Square test to analyze the relationship
+        between two categorical variables.
        
        
-       Parameters
-       ----------
-       column1, column2: str
-           The names of the columns to analyze. These should be two strings 
+        Parameters
+        ----------
+        column1, column2: str
+           The names of the columns to analyze. These should be two strings
            corresponding to the column names in the Titanic dataset.
         
         
-       Returns
-       -------
-       chi2: float
+        Returns
+        -------
+        chi2: float
            The Chi-Square statistic which measures the difference between
            observed and expected frequencies.
-       p: float 
+        p: float
            The p-value indicating statistical significance.
-       v: float
-           Cramer's V value which measures the strength of association 
-           between the two variables. 
+        v: float
+           Cramer's V value which measures the strength of association
+           between the two variables.
        
            
-       Notes
-       -----
-       The Chi-Square test is only used for categorical variables. 
-       A p-value < 0.05 indicates a statistically significant relationship 
-       between the two categorical variables. 
-       Cramer's V value ranges from 0 to 1. Values close to 0 indicate weak 
-       association and values near 1 indicate a strong association.
+        Notes
+        -----
+        The Chi-Square test is only used for categorical variables.
+        A p-value < 0.05 indicates a statistically significant relationship
+        between the two categorical variables.
+        Cramer's V value ranges from 0 to 1. Values close to 0 indicate weak
+        association and values near 1 indicate a strong association.
        
        
-       Examples
-       --------
-       analyzer.chi_square_test(column1 = "pclass", column2 = "survived")
+        Examples
+        --------
+        analyzer.chi_square_test(column1 = "pclass", column2 = "survived")
         
-       Ouptput:
-       There is a statistically significant relationship between pclass and survived
-       Out[13]: (102.88898875696056, 4.549251711298793e-23, 0.33981738800531175)                    
-          
+        Ouptput:
+        There is a statistically significant relationship between pclass and survived
+        Out[13]: (102.88898875696056, 4.549251711298793e-23, 0.33981738800531175)
         
-       """
-       contigency_table = pd.crosstab(self.data[column1], self.data[column2])
-       chi2, p, dof, expected = chi2_contingency(contigency_table)
-       v = self.cramers_v(chi2, contigency_table)
+        """
+        contigency_table = pd.crosstab(self.data[column1], self.data[column2])
+        chi2, p, dof, expected = chi2_contingency(contigency_table)
+        v = self.cramers_v(chi2, contigency_table)
 
-       alpha = 0.05
-       if p < alpha: print(f"There is a statistically significant relationship between {column1} and {column2}")
-       else: print(f"\n There is no significant relationship between {column1} and {column2} \n")
+        alpha = 0.05
+        if p < alpha: print(f"There is a statistically significant relationship between {column1} and {column2}")
+        else: print(f"\n There is no significant relationship between {column1} and {column2} \n")
        
-       return chi2, p, v
+        return chi2, p, v
     
     
     
@@ -160,27 +159,22 @@ class Analyzer:
         return pivot_table_count
     
 
-    def survival_rate(self, group_by_column : str):
+    def survival_rate(self, group_by_column: str):
         """ computes survival rates grouped by a specific column (categorial variable)"""
         survival_rates = self.data.groupby(group_by_column)["survived"].mean()
         return survival_rates
-    
-    
-# %%
-def all_sex(data):
-    all_male = sum(data["Sex"]== "male")
-    all_female = sum(data["Sex"]== "female")
-# Diese Funktion ist einfach hier. Ich weiß jetzt nicht ob die relevant sein könnte deswegen steht diese hier.
+
+
 # %%
 analyzer = Analyzer(data)
 # %%
 # use the ccf_categorization method to analyze the average fare by embark town
-# and passenger class and the passenger count by embark town and passenger class
+# and passenger class and the passenger count by embark town and passenger "class"
 
 analyzer.ccf_categorization_mean()
 # Average fare by embark town and passenger class:
 # pclass                1          2          3
-# embark_town                                  
+# embark_town
 # Cherbourg    104.718529  25.358335  11.214083
 # Queenstown    90.000000  12.350000  11.183393
 # Southampton   70.364862  20.327439  14.644083
@@ -188,7 +182,7 @@ analyzer.ccf_categorization_mean()
 analyzer.ccf_categorization_count()
 # Passenger count by embark town and passenger class:
 # pclass         1    2    3
-# embark_town               
+# embark_town
 # Cherbourg     85   17   66
 # Queenstown     2    3   72
 # Southampton  127  164  353
@@ -196,7 +190,7 @@ analyzer.ccf_categorization_count()
 # %%
 # use the survival_rate method to compute survival rates
 # grouped by a categorial variable
-# here are some examples 
+# here are some examples
 
 analyzer.survival_rate("who")
 # who
@@ -209,9 +203,5 @@ analyzer.survival_rate("pclass")
 # 1    0.629630
 # 2    0.472826
 # 3    0.242363
-
-
-
-
-
-
+# %%
+analyzer.chi_square_test("age", "fare")
